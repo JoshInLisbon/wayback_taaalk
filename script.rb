ef756b3c8826f0c08adb192a374407182cb0994b
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2022-02-26 12:30:24 +0000

    progress on script.rb

--- script.rb
+++ script.rb
@@ -1,10 +1,32 @@
 require 'nokogiri'
 
-Dir.each_child("t") do |d|
-  dir = "t/" + d
-  puts dir
-  Dir.each_child(dir) do |f|
-    file = File.open(f)
-    puts Nokogiri::HTML(file)
+taaalks = Dir["t/*/index.html"].map do |path|
+  page = Nokogiri::HTML(File.open(path))
+
+  speakers = page.css('.spkr-info').map do |spkr_info|
+    info_header = spkr_info.css('h3 a')[0]
+    {
+      name: info_header.text,
+      id: info_header.attr('class').delete("^0-9"),
+      side: spkr_info.attr('class').gsub('spkr-info spkr-info-',''),
+      profile_path: info_header['href'],
+      twitter_handle: spkr_info.css('.twitter-handle').text.strip,
+      bio: spkr_info.css('.trix-content').inner_html,
+    }
   end
+
+  messages = page.css('.tlk-blob').map do |message|
+    {
+      speaker_id: message.css('div[class^="name-spkr-"]')[0].attr('class').delete("^0-9"),
+      created_at: message.css('div[class="tlk-blob-date"]').text.partition("(")[0].strip,
+    }
+  end
+
+  {
+    title: page.css('h1')[0].text,
+    speakers: speakers,
+    messages: messages,
+  }
 end
+
+pp taaalks
